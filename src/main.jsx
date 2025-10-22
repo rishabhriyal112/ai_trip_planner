51b3713f70827c1b3dfd922729a56a251d2b9e3a
User <riyalrishabh22@gmail.com>
2025-10-22 17:28:56 +0530

    fix issues

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import CreateTrip from './create-trip/index.jsx'
 import Header from './components/custom/Header.jsx'
 import { Toaster } from 'sonner'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import Viewtrip from './view-trip/tripId/index.jsx'
+import Viewtrip from './view-trip/[tripId]/index.jsx'
 import MyTrips from './my-trips/index.jsx'
 
 
